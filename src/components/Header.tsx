320499cd87bd19e99c9f984d479075ba9472913e
Tom Bazarnik <tommywalkie@gmail.com>
2025-02-19 00:52:35 +0100

    tweaked dark mode toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export function Header() {
 
       <button
         onClick={toggleDarkMode}
-        className="p-2 rounded-lg hover:bg-primary-600 hover:text-white dark:hover:bg-primary-600 cursor-pointer ring-none focus:ring-none"
+        className="p-2 rounded-lg bg-primary-100 dark:bg-primary-700 hover:bg-primary-200 dark:hover:text-white dark:hover:bg-primary-600 cursor-pointer ring-none focus:ring-none mr-2"
         aria-label={isDark ? 'Switch to light mode' : 'Switch to dark mode'}
       >
         {isDark ? <Sun className="w-5 h-5 text-white" /> : <Moon className="w-5 h-5" />}
